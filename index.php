c4f332298f2e3bdde6fffde8bcd3ca0cf7f1dc27
xVrzBx <bryancs4899@gmial.com>
2022-08-12 17:41:22 -0500

    Partial html

--- index.php
+++ index.php
@@ -3,58 +3,7 @@
   $contacts = $conn->query("SELECT * FROM contacts");
   
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- bootstrap -->
-      <link rel="stylesheet" 
-            href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css" 
-            integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w==" 
-            crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <script 
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-            crossorigin="anonymous"></script>
-
-      <link rel="stylesheet" href="./static/css/index.css">
-      <title>SI, DOS DOS</title>
-</head>
-<body>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-              <a class="navbar-brand font-weight-bold" href="#">
-                <img class="mr-2" src="./static/img/logo.png" />
-                ContactsApp
-              </a>
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#/">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="add.php">Add Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-          <main>
+<?php require "partials/header.php" ?>
             <div class="container pt-4 p-3">
                   <div class="row">
                     <?php if ($contacts->rowCount() == 0): ?>
@@ -79,6 +28,5 @@
                     <?php endforeach ?>
                   </div>
             </div>
-          </main>
-</body>
-</html>
+<?php require "footer.php"?>
+
