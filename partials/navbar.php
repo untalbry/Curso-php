0a4e6e4af3929311a013fdfad80a40dd0a29b0ec
xVrzBx <bryancs4899@gmial.com>
2022-09-02 16:29:22 -0500

    Create user

--- partials/navbar.php
+++ partials/navbar.php
@@ -17,12 +17,19 @@
               </button>
               <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
+                  <!-- <li class="nav-item">
+                    <a class="nav-link" href="home.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="add.php">Add Contact</a>
+                  </li> -->
                   <li class="nav-item">
                     <a class="nav-link" href="home.php">Home</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="add.php">Add Contact</a>
                   </li>
+                  
                 </ul>
               </div>
             </div>
